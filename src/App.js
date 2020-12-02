5dfdb51cd4cf6764e1b467f307b885d327a07bb3
Arina768 <chertkovaarina@gmail.com>
2020-12-02 21:26:30 +0300

    app created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import data from './info.json';
+import { Input, Checkbox, Button } from 'antd';
+import 'antd/dist/antd.css';
+
+export default function App () {
+  const [list, setList] = useState([]);
+  const [caseSensitive, setCaseSensitive] = useState(false);
+  const [filteredList, setFilteredList] = useState([]);
+  const [criterion,setCriterion] = useState('');
+
+  useEffect(() => {
+      async function fetchList() {
+        const response = await fetch('https://www.mrsoft.by/data.json');
+        const fullList = await response.json().data
+        // const fullList = data.data;
+        setList(fullList)
+        // setDisplayList(true)
+      }
+      fetchList()
+    })
+
+  const changeCriterion = ({target}) => {
+    setCriterion(target.value)
+  }
+
+  const filterByLenght = () => {
+    const filteredList = list.filter(string => string.length > criterion);
+    setFilteredList(filteredList);
+  }
+
+  const filterByString = () => {
+    if(caseSensitive) {
+      const filteredList = list.filter(string => string.indexOf(criterion) !== -1);
+      setFilteredList(filteredList);
+    } else {
+      const filteredList = list.filter(string => string.toLowerCase().indexOf(criterion.toLowerCase()) !== -1);
+      setFilteredList(filteredList);
+    }
+  }
+
+  const changeCaseSensitivity = ({target}) => {
+    setCaseSensitive(target.checked);
+  }
+
+return (
+  <>
+  <div className='input-container'>
+    <Input className='input' onChange={changeCriterion}/>
+    <Button type='primary' onClick={filterByLenght}> Отфильтровать по длине </Button>
+    <Button type='primary' onClick={filterByString}>Отфильтровать по подстроке</Button>
+  </div>
+  <div className='checkbox-container'>
+    <Checkbox onChange={changeCaseSensitivity}> Учитывать регистр</Checkbox>
+  </div>
+    <ul> {
+        filteredList.length
+        ? filteredList.map(item => <li>{item}</li>)
+          :null
+    }
+    </ul>
+    </>
+)
 }
-
-export default App;
