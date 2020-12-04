e2cfe5843921bf08cb462c831f3e7b8646faee6a
Arina768 <chertkovaarina@gmail.com>
2020-12-04 20:04:03 +0300

    app created

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
-import data from './info.json';
 import { Input, Checkbox, Button } from 'antd';
 import 'antd/dist/antd.css';
 
-export default function App () {
+export default function App() {
   const [list, setList] = useState([]);
   const [caseSensitive, setCaseSensitive] = useState(false);
   const [filteredList, setFilteredList] = useState([]);
-  const [criterion,setCriterion] = useState('');
+  const [criterion, setCriterion] = useState('');
 
   useEffect(() => {
-      async function fetchList() {
-        const response = await fetch('https://www.mrsoft.by/data.json');
-        const fullList = await response.json().data
-        // const fullList = data.data;
-        setList(fullList)
-        // setDisplayList(true)
-      }
-      fetchList()
-    })
+    async function fetchList() {
+      const url = 'https://www.mrsoft.by/data.json';
+      const proxyUrl = 'https://cors-anywhere.herokuapp.com/';
+      const response = await fetch(proxyUrl + url);
+      const result = await response.json();
+      const fullList = result.data;
+      setList(fullList)
+    }
 
-  const changeCriterion = ({target}) => {
+    fetchList()
+  }, [0])
+
+  const changeCriterion = ({ target }) => {
     setCriterion(target.value)
   }
 
@@ -31,7 +32,7 @@ export default function App () {
   }
 
   const filterByString = () => {
-    if(caseSensitive) {
+    if (caseSensitive) {
       const filteredList = list.filter(string => string.indexOf(criterion) !== -1);
       setFilteredList(filteredList);
     } else {
@@ -40,26 +41,26 @@ export default function App () {
     }
   }
 
-  const changeCaseSensitivity = ({target}) => {
+  const changeCaseSensitivity = ({ target }) => {
     setCaseSensitive(target.checked);
   }
 
-return (
-  <>
-  <div className='input-container'>
-    <Input className='input' onChange={changeCriterion}/>
-    <Button type='primary' onClick={filterByLenght}> Отфильтровать по длине </Button>
-    <Button type='primary' onClick={filterByString}>Отфильтровать по подстроке</Button>
-  </div>
-  <div className='checkbox-container'>
-    <Checkbox onChange={changeCaseSensitivity}> Учитывать регистр</Checkbox>
-  </div>
-    <ul> {
+  return (
+    <>
+      <div className='input-container'>
+        <Input className='input' onChange={changeCriterion}/>
+        <Button type='primary' onClick={filterByLenght}> Отфильтровать по длине </Button>
+        <Button type='primary' onClick={filterByString}>Отфильтровать по подстроке</Button>
+      </div>
+      <div className='checkbox-container'>
+        <Checkbox onChange={changeCaseSensitivity}> Учитывать регистр</Checkbox>
+      </div>
+      <ul> {
         filteredList.length
-        ? filteredList.map(item => <li>{item}</li>)
-          :null
-    }
-    </ul>
+          ? filteredList.map(item => <li>{item}</li>)
+          : null
+      }
+      </ul>
     </>
-)
+  )
 }
